3ec91140cad3f119ffab3ba402018660c82d3324
Seryozha <arzumanyan.seroj@mail.ru>
2022-12-11 20:12:05 +0300

    test URL for vercel2

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     // origin: (process.env.REACT_APP_URL || 'http://localhost:3000'),
-    origin: ('https://auth-front-eta.vercel.app'),
+    origin: 'https://auth-front-eta.vercel.app',
     credentials: true
 }));
 
